fac5ec067207366e15f887d8c66e8f94d1c2aeb4
Alexis <alexisdager93@hotmai.com>
2019-10-26 11:29:32 -0500

    Navbar . service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './Components/nav-bar/nav-bar.component';
+import { AuthComponent } from './Service/auth/auth.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    AuthComponent
   ],
   imports: [
     BrowserModule,
